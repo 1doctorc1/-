cf2be2950a0b4fdd9c7b5ffb4ef8d808fc96e92a
zhangchi <2281781492@qq.com>
2020-11-19 20:56:20 +0800

    导论课大作业

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,20 @@
+const app = getApp()
+Page({
+    data: {
+        kaipingFlag: true
+    },
+    onLoad: function () {
+        //wx.hideTabBar();
+    },
+    onMyEvent: function () {
+        console.log("close");
+        this.setData({
+            kaipingFlag: false
+        });
+        //wx.showTabBar();
+        wx.navigateBack({
+          url: '../diy/diy',
+        })
+    }
+    
+})
